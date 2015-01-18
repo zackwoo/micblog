6f754bd3f55b993b038924ddfb38312a7fe5b80c
zackwoo <zachary.woo@163.com>
2015-01-18 15:00:11 +0800

    update

--- test/a.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs-extra');
-var should = require('chai').should();
-
-fs.readFile('micblogDeploy.md', 'utf-8', function (err, data) {
-    if (err) throw err;
-
-    console.log(data.replace(/^#([^#].+)/, ""));
-});
